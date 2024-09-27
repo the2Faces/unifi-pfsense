17e0a17da23f94f970aefde248e11ea4f011bd43
the2Faces <the2faces@web.de>
2024-09-27 12:20:04 +0200

    Update install-unifi.sh
    Reverted to latest official version of Unifi Controller 8.4.6

--- install-unifi/install-unifi.sh
+++ install-unifi/install-unifi.sh
@@ -4,7 +4,7 @@
 # Installs the Uni-Fi controller software on a FreeBSD machine (presumably running pfSense).
 
 # The latest version of UniFi:
-UNIFI_SOFTWARE_URL="https://dl.ui.com/unifi/8.5.5-b8u0px98kb/UniFi.unix.zip"
+UNIFI_SOFTWARE_URL="https://dl.ui.com/unifi/8.4.62-i3q2j125cz/UniFi.unix.zip"
 
 
 # The rc script associated with this branch or fork:
